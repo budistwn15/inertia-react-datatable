56ff9ec425d74ab418c5f501988e35292a225ba8
budistwn15 <budi.setiawan112001@gmail.com>
2023-12-08 16:54:25 +0700

    user collection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return User::get();
+        return UserResource::collection(User::paginate(10));
     }
 }
